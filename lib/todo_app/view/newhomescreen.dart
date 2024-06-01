9a7bd6e1895a4bc7d27b04f0e872bd0b1ab74e85
Duc03052003 <duc.na.63cntt@ntu.edu.vn>
2024-06-01 08:36:32 +0700

    home todo

--- lib/todo_app/view/newhomescreen.dart
+++ lib/todo_app/view/newhomescreen.dart
@@ -1,125 +1,161 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:project_final/todo_app/model/task.dart';
-import 'package:provider/provider.dart';
 import 'package:project_final/todo_app/controller/task_controller.dart';
+import 'package:provider/provider.dart';
 
-class NewHomeScreen extends StatefulWidget {
-  const NewHomeScreen({super.key});
+class AddNewTask extends StatefulWidget {
+  final String? topic;
+  const AddNewTask({Key? key, this.topic}) : super(key: key);
+
 
   @override
-  State<NewHomeScreen> createState() => _NewHomeScreenState();
+  State<AddNewTask> createState() => _AddNewTaskState();
 }
 
-class _NewHomeScreenState extends State<NewHomeScreen> {
+class _AddNewTaskState extends State<AddNewTask> {
   TextEditingController txtnameTask = TextEditingController();
+
+  void _showInputDialog(BuildContext context, {ToDoSnapshot? snapshot}) {
+    if (snapshot != null) {
+      txtnameTask.text = snapshot.toDoTask.taskName!;
+    } else {
+      txtnameTask.clear();
+    }
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Nhập task"),
+          content: TextField(
+            controller: txtnameTask,
+            decoration: InputDecoration(labelText: 'Nhập task'),
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Hủy'),
+            ),
+            TextButton(
+              onPressed: () async {
+                if (txtnameTask.text.isNotEmpty) {
+                  String newTaskName = txtnameTask.text;
+                  if (snapshot != null) {
+                    // Update existing task
+                    await snapshot.ref.update({
+                      'taskName': newTaskName,
+                      'createBy': 'Bao',
+                      'createAt': DateTime.now(),
+                      'isCompleted': snapshot.toDoTask.isCompleted,
+                    });
+                  } else {
+                    // Add new task
+                    await FirebaseFirestore.instance.collection("ToDoDB").add({
+                      'taskName': newTaskName,
+                      'createBy': 'Bao',
+                      'createAt': Timestamp.now(),
+                      'isCompleted': false,
+                    });
+                  }
+                  Navigator.of(context).pop();
+                }
+              },
+              child: Text('Lưu'),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home Page"),
+        title: Text("Tasks"),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              _showInputDialog(context);
+            },
+          ),
+        ],
       ),
-
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Row(
-              children: [
-                Expanded(
-                    child: Container(
-                      height: 55,
-                      width: MediaQuery.of(context).size.width,
-                      child: TextFormField(
-                        controller: txtnameTask,
-                        decoration: InputDecoration(
-                          hintText: "Nhap nhiem vu moi",
-                        ),
-                        validator: (value){
-                          if (value == null || value.isEmpty) {
-                            return 'Vui long nhap nhiem vu';
-                          }
-                          return null;
-                        },
-                      ),
-                    )),
-                FloatingActionButton(
-                  onPressed: () {
-                    if(txtnameTask.text.isNotEmpty){
-                      String newtaskName = txtnameTask.text;
-                      FirebaseFirestore.instance.collection("ToDoDB").add({
-                        'taskName': newtaskName,
-                        'createBy': 'Bao',
-                        'createAt': DateTime.now(),
-                        'isCompleted': false,
-                      });
-                      txtnameTask.clear();
-                    }
-                  },
-                  child: const Icon(Icons.add),
-                ),
-              ],
-            ),
-          ),
           Expanded(
             child: StreamBuilder(
               stream: FirebaseFirestore.instance.collection("ToDoDB").snapshots(),
-              builder: (context, snapshot) {
+              builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
                 if (snapshot.hasError) {
-                  return Center(child: Text("Error Data"),);
+                  return Center(child: Text("Error Data"));
                 }
                 if (!snapshot.hasData) {
-                  return Center(child: CircularProgressIndicator(),);
+                  return Center(child: CircularProgressIndicator());
                 }
+
                 var docs = snapshot.data!.docs;
                 var toDoSnapshotList = docs.map((doc) => ToDoSnapshot.fromMap(doc)).toList();
-                // toDoSnapshotList.sort((a, b) => a.toDoTask.createAt!.compareTo(b.toDoTask.createAt!));
 
                 return ListView.builder(
                   itemCount: toDoSnapshotList.length,
                   itemBuilder: (context, index) {
-                    var toDoSnapshot = ToDoSnapshot.fromMap(docs[index]);
+                    var toDoSnapshot = toDoSnapshotList[index];
                     return Dismissible(
-                        key: Key(toDoSnapshot.toDoTask.taskName!),
-                        direction: DismissDirection.endToStart,
-                        onDismissed: (direction) async {
-                          await toDoSnapshot.xoa();
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              content: Text('Đã xóa 1 nhiệm vụ'),
-                            ),
-                          );
-                        },
-                        background: Container(
-                          color: Colors.red,
-                          alignment: Alignment.centerRight,
-                          padding: const EdgeInsets.symmetric(horizontal: 20),
-                          child: const Icon(
-                            Icons.delete,
-                            color: Colors.white,
+                      key: Key(toDoSnapshot.toDoTask.taskName!),
+                      direction: DismissDirection.endToStart,
+                      onDismissed: (direction) async {
+                        await toDoSnapshot.xoa();
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text('Đã xóa 1 nhiệm vụ'),
+                          ),
+                        );
+                      },
+                      background: Container(
+                        color: Colors.red,
+                        alignment: Alignment.centerRight,
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: const Icon(
+                          Icons.delete,
+                          color: Colors.white,
+                        ),
+                      ),
+                      child: ListTile(
+                        trailing: Checkbox(
+                          value: toDoSnapshot.toDoTask.isCompleted!,
+                          onChanged: (value) async {
+                            if (value != null) {
+                              await ToDoSnapshot.updateTaskStatus(toDoSnapshot.ref, value);
+                            }
+                          },
+                        ),
+                        title: Text(
+                          toDoSnapshot.toDoTask.taskName!,
+                          style: TextStyle(
+                            decoration: toDoSnapshot.toDoTask.isCompleted!
+                                ? TextDecoration.lineThrough
+                                : TextDecoration.none,
                           ),
                         ),
-                        child: ListTile(
-                          trailing: Checkbox(
-                            value: toDoSnapshot.toDoTask.isCompleted!,
-                            onChanged: (value) async {
-                              await ToDoSnapshot.updateTaskStatus(docs[index].reference, value!);
-                            },
-                          ),
-                          title: Text(toDoSnapshot.toDoTask.taskName!,
-                            style: TextStyle(
-                              decoration: toDoSnapshot.toDoTask.isCompleted! ? TextDecoration.lineThrough : TextDecoration.none,
-                            ),),
-                          subtitle: Text('Created at: ${DateFormat('dd/MM/yy HH:mm').format(toDoSnapshot.toDoTask.createAt!.toDate())}'),
-                        ));
+                        subtitle: Text(
+                          'Created at: ${DateFormat('dd/MM/yy HH:mm').format(toDoSnapshot.toDoTask.createAt!.toDate())}',
+                        ),
+                        onTap: () {
+                          _showInputDialog(context, snapshot: toDoSnapshot);
+                        },
+                      ),
+                    );
                   },
                 );
               },
-            )
+            ),
           ),
         ],
       ),
